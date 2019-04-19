096e3dfc2fe3d3c7d94187f0b75734f508a8625c
= <=>
2019-04-19 21:21:00 +0100

    added config.yml, verifyUser plugin, admin/

---
+++ plugins/verifyUser.js
@@ -0,0 +1,15 @@
+export default () => {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      } else {
+        console.log("!user evaluates to true");
+      }
+    });
+  } else {
+    console.log("window.netlifyIdentity evaluates to false");
+  }
+};
